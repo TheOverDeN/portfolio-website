1946c50f3f8c72463d2cc7e507a0936dcca2536c
TheOverDeN <overdenek39@gmail.com>
2023-12-07 14:03:30 +0100

    api

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -51,7 +51,7 @@ const AboutSection = () => {
   return (
     <section className='text-white' id="about">
         <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-            <Image src="/images/aboutmepic3.jpg" width={500} height={500}/>
+            <Image src="/images/aboutmepic3.jpg" width={500} height={500} alt="tekst"/>
             <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
                 <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
                 <p className='text-base md:text-lg'> I am a full stack web developer with a passion for creating interactive and responsive web applications. I have experience working with Javascript, React, Redux, Node.js, mySQL, HTML, CSS. I am a quick learner and I am always lookng to expand my knowledge and skill set. I am a team player and i am excited to work with others to create amazing applications.</p>
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -44,7 +44,7 @@ const HeroSection = () => {
         <div className="rounded-full bg-[#181818] w-[350px] h-[350px] lg:w-[400px] lg:h-[400px] relative">
           <Image
           src="/images/logomountain.png"
-          alt="hero image"
+          alt="hero-image"
           className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
           width={300} 
           height={300} />
