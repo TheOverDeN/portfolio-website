05dc2f9b64f5d9b542afa97332b72ccd361ee269
TheOverDeN <overdenek39@gmail.com>
2023-12-07 13:45:18 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
+webpack: config => {
+    config.ignoreWarnings = [
+        { module: /node_modules\/node-fetch\/lib\/index\.js/ },
+        { file: /node_modules\/node-fetch\/lib\/index\.js/ },
+      ];
+    
+      return config;
+    }
 
 module.exports = nextConfig
